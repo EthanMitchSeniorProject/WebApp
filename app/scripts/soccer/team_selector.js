698a24937a9821627560dfd78a3265676ceeb905
MitchStark10 <mitch.stark.github@gmail.com>
2017-11-25 15:22:45 -0500

    fixed tedious singular connection, added in team selector (needs css)

--- app/scripts/soccer/team_selector.js
+++ app/scripts/soccer/team_selector.js
@@ -4,27 +4,43 @@ import $ from 'jquery';
 import styles from '../../css/drop_down.css';
 
 class TeamSelector extends React.Component {
+    constructor(props) {
+        super(props);
+        this.setState = this.setState.bind(this);
+        this.getAllTeams = this.getAllTeams.bind(this);
+        this.state = {teams: []};
+        this.getAllTeams();
+    }
+
     getAllTeams = () => {
         let render_teams = [];
-        $.getJSON("/soccer/teams", function (teams) {
+        $.getJSON("/soccer/teams", (teams) => {
             for (var i = 0; i < teams.length; i++) {
                 let current_team = teams[i];
                 console.log("/soccer/teams/" + current_team['id'] + "/games");
-                $.getJSON("/soccer/teams/" + current_team['id'] + "/games", function(games) {
+                $.getJSON("/soccer/teams/" + current_team['id'] + "/games", (games) => {
                     if (games.length > 5) {
                         render_teams.push(current_team['school_name']);
                         console.log("Adding" + current_team['school_name'] + " with " + games.length + " games");
+                        let newTeams = this.state.teams;
+                        this.state.teams.push(current_team['school_name']);
+                        this.setState({"teams" : newTeams});
                     } else {
                         console.log("Not adding " + current_team['school_name'] + " with " + games.length + " games");
                     }
                 });
             }
         });
-        //make request to soccer/teams
-        //for each item, add <a href="#">Team</a>
-        return (
-            <h1>Hi</h1>
-        )
+    }
+
+
+    renderTeams = () => {
+        return this.state.teams.map(function(team) {
+            console.log("rendering: " + team);
+            return (
+                <input className="select_button" type="submit" value={team}/>
+            )
+        })
     }
 
     render = () => {
@@ -32,7 +48,7 @@ class TeamSelector extends React.Component {
             <div className={styles.dropdown}>
                 <button className={styles.dropbtn}>Select Team</button>
                 <div className={styles.dropdown_content}>
-                    {this.getAllTeams()}
+                    {this.renderTeams()}
                 </div>
             </div>
         )
