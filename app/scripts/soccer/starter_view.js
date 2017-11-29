22c2d2c863c3a24498187eb884a22d026ad78288
MitchStark10 <MitchStark10@gmail.com>
2017-11-28 21:37:15 -0500

    starter view created and working

--- app/scripts/soccer/starter_view.js
+++ app/scripts/soccer/starter_view.js
@@ -1,26 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import styles from '../../css/drop_down.css';
-import base_styles from '../../css/base.css';
+import styles from '../../css/base.css';
 
 class StarterView extends React.Component {
     constructor(props) {
         super(props);
         this.setState = this.setState.bind(this);
+        this.state = {"starter_jsx": null}
+        this.most_recent_team = props.team;
     }
 
     getStarters = () => {
+        if ((this.state.starter_jsx != null) && (this.props.team == this.most_recent_team)) {
+            return;
+        }
+
+        this.most_recent_team = this.props.team;
+
         $.getJSON('/soccer/teams/' + this.props.team + '/team_id', (response_arr) => {
             let team_id = response_arr[0]['id'];
             $.getJSON('/soccer/teams/' + team_id + '/starters', (starter_list) => {
-                console.log(starter_list);
+                console.log("starters: " + JSON.stringify(starter_list));
+                this.setState({starter_jsx: 
+                 (
+                    <div className="starter_table_div">
+                        <table className="starter_table">
+                            <tr>
+                                <th>Name</th>
+                                <th>Number</th>
+                            </tr>
+                            {
+                                starter_list.filter(player_json => player_json['starting_ratio'] == 1).map(function(player_json) {
+                                    return (
+                                        <tr>
+                                            <td>{player_json['name']}</td>
+                                            <td>{player_json['num']}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </table>
+                    </div>
+                )                  
+                });
             });
         });
-
-        return (
-            <p>Starters</p>
-        )
     }
 
     render = () => {
@@ -28,11 +53,13 @@ class StarterView extends React.Component {
             return null;
         }
 
+        this.getStarters();
+
         return (
             <div className="starterView">
                 <h1>{this.props.team}: Consistent Starters</h1>
                 <div className="starters">
-                    {this.getStarters()}
+                    {this.state.starter_jsx}
                 </div>
             </div>
         )
