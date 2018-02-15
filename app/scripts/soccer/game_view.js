4ed95dd375c6b9d8b3d138c5d95832d07f1fb10c
Mitch Stark <mitch.stark.github@gmail.com>
2018-02-15 08:58:59 -0500

    began next step of a game view

---
+++ app/scripts/soccer/game_view.js
@@ -0,0 +1,14 @@
+
+
+class GameView extends React.Component {
+    constructor(props) {
+        this.setState = this.setState.bind(this);
+        this.most_recent_team = props.team;
+    }
+
+    render = () => {
+        return null;
+    }
+}
+
+export default GameView;
